b2326adfbeb2485dc56e38edb12f31145a1e7570
alejandro_leno <alexlorenzo39@gmail.com>
2019-05-17 10:28:51 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from config import DevConfig
+
+
+app = Flask(__name__)
+app.config.from_object(DevConfig)
+
+
+@app.route
+def home():
+    return '<h1>welcome home</h1>'
+
+if __name__ == '__main__':
+    app.run()
