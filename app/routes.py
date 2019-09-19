5344ee44dcbcfc75fdf2e42fad7a69ed8ce1b115
amloren1 <alexlorenzo39@gmail.com>
2019-09-18 19:40:02 -0700

    login required

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for, request
-from flask_login import login_user, logout_user, current_user
+from flask_login import login_user, logout_user, login_required,  current_user
 from flask_sqlalchemy import SQLAlchemy
 #from config import DevConfig
 from app.forms import LoginForm, PlanetParamsForm
@@ -82,5 +82,6 @@ def logout():
     return redirect(url_for("home"))
 
 @app.route("/cam_query")
+@login_required
 def cam_query():
     return render_template("cam_query.html")
