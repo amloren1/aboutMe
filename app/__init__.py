33dffb5240ba9834580506a9bef80f27c7b80a07
amloren1 <alexlorenzo39@gmail.com>
2019-09-20 18:58:29 -0700

    buggybug

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+#migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = "info"
