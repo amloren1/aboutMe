be14df81974f68d9986b2b8c9d4d012fe56a5ef2
amloren1 <alexlorenzo39@gmail.com>
2019-09-20 18:57:02 -0700

    update requirements.txt

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
-from flask_migrate import Migrate
+#from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
