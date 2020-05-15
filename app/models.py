0f52fd3c6e8f8912db5456e2d2f09a5ddac292ff
amloren1 <alexlorenzo39@gmail.com>
2020-05-15 13:32:51 -0700

    add password hashing, change user db column name for password to password_hash

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 import datetime
 
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from app import db, login_manager
 
@@ -20,11 +21,17 @@ def load_user(user_id):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20), unique = True, nullable = False) # no duplicates allowed
-    password = db.Column(db.String(10), unique = True, nullable = False)
+    password_hash = db.Column(db.String(10), unique = True, nullable = False)
 
     def __repr__(self):
         # useful for printing details of the class instance
         return f"<User('{self.username}')>"
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
