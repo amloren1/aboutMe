d8e6f0cdd0a5fa51ae408172cee9a5164df5a339
amloren1 <alexlorenzo39@gmail.com>
2020-05-18 15:49:13 -0700

    add some comments

--- app/routes.py
+++ app/routes.py
@@ -143,12 +143,14 @@ def user(username):
 def edit_profile():
     form = EditProfileForm()
     if form.validate_on_submit():
+        # set current user data
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
         db.session.commit()
         flash('Your changes have been saved.')
         return redirect(url_for('edit_profile'))
     elif request.method == 'GET':
-        form.username.data = current_user.username
+        # display the current profile (allows you to change username too
+        form.username.data = current_user.username 
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
