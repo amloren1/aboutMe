2554c8e84e79cea76db7c69ffdc659bf0f5e53d7
amloren1 <alexlorenzo39@gmail.com>
2020-05-18 16:52:16 -0700

    error handler and log file output

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
 from flask import Flask
 from flask_login import LoginManager
 from flask_migrate import Migrate
@@ -12,5 +16,17 @@ migrate = Migrate(app, db) # database migration engine
 login_manager = LoginManager(app)
 login_manager.login_view = 'login' # login endpoint
 login_manager.login_message_category = "info"
+
+if not app.debug:
+    if not os.path.exists("logs"):
+            os.mkdir("logs")
+    file_handler = RotatingFileHandler("logs/aboutME.log", maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('aboutMe startup')
 
 from app import routes, models, errors #models will define the structure of the database
